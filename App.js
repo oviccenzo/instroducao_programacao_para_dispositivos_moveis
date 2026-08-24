1c5baefa7afb95c429c5d87bde27f7d2db71cf5a
Viccenzo de Oliveira Nunes Resende <viccenzo.oliveira@gmail.com>
2026-08-24 11:25:08 -0300

    Projeto limpo configurado com JavaScript puro

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Viccenzo: Bacharel ciencia da computacao</Text>
       <StatusBar style="auto" />
     </View>
   );
